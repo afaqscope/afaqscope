baa526773ae3586f9f8ba78a53ae83df17711173
makahleh <mohamed.makahleh@yahoo.com>
2018-03-30 19:47:56 +0300

    initial

---
+++ AccessDB_Test/AccessDB/Access.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Access
+{
+    private Log logger = new Log();
+    private String connectionString = @"Provider=Microsoft.JET.OlEDB.4.0;"
+                       + @"Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "LocalStorage.accdb";
+    public bool testConnection()
+    {
+        try
+        {
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.writeToLog("testConnection", ex.StackTrace, ex.Message);
+            return false;
+        }
+    }
+
+    public bool executeCommand(string commandString, ref List<string> result)
+    {
+        try
+        {
+                DataSet ds = new DataSet();
+                using (OleDbConnection conn = new OleDbConnection(connectionString))
+                {
+                    conn.Open();
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(commandString, conn);
+                    da.Fill(ds);
+                    conn.Close();
+                    DataTable dt = ds.Tables[0];
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        foreach (string value in row.ItemArray)
+                        {
+                            result.Add(value);
+                        }
+                    }
+                }           
+           
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.writeToLog("testConnection", ex.StackTrace, ex.Message);
+            return false;
+        }
+    }
+
+    public bool executeCommand(string commandString)
+    {
+        try
+        {
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand command = new OleDbCommand(commandString, conn);
+                command.ExecuteNonQuery();
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.writeToLog("testConnection", ex.StackTrace, ex.Message);
+            return false;
+        }
+    }
+
+
+
+}
+
---
+++ AccessDB_Test/AccessDB_Test/json.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AccessDB_Test
+{
+    class JSON
+    {
+        string jsonFile = AppDomain.CurrentDomain.BaseDirectory + "data.json";
+        dynamic data;
+        public JSON()
+        {
+            if (File.Exists(jsonFile))
+            {
+                string json = File.ReadAllText(jsonFile);
+                data = JsonConvert.DeserializeObject(json);
+            }
+        }
+        private List<SelectListItem> getBeneficiaryTypes()
+        {
+            for (int index = 0; index < data.settings.householdForm.beneficiaryType.choices.length; index++)
+            {
+                List<SelectListItem> beneficiaryTypes = new List<SelectListItem>();
+                beneficiaryTypes.Add(new SelectListItem
+                {
+                    Text = data.settings.householdForm.beneficiaryType.choices[index],
+                    Value = index
+                });
+            }
+            return beneficiaryTypes;
+        }
+    }
+}
