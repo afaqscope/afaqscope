e41b7f16659efae6b42b3c240ab4524952bac6e7
makahleh <mohamed.makahleh@yahoo.com>
2018-04-05 00:15:08 +0300

    Commit : WebService

--- Server/WebService/IService.cs
+++ Server/WebService/IService.cs
@@ -13,7 +13,7 @@ namespace WebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IWebService
+    public interface IService
     {
 
         [OperationContract(Name = "startHouseHoldWork")]
